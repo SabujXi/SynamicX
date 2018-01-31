d8e00164d54dde3fb10dae319191114f1242226c
sabuj <md.sabuj.sarker@gmail.com>
2018-02-01 01:36:05 +0600

    Updated new filtering system

--- src/synamic/core/new_filter/filter_functions.py
+++ src/synamic/core/new_filter/filter_functions.py
@@ -3,35 +3,53 @@ texts | title.length > 1 | title contains "yo yo" | tags contains ["done", "comp
 """
 import re
 from pprint import pprint
+from collections import namedtuple
 
 module_name_pat = re.compile(r"^(?P<module_name>[a-zA-Z0-9_-]+)", re.IGNORECASE)
 pipe_pat = re.compile(r"[\s]*\|[\s]*")
 
 field_name_pat = re.compile(r"^(?P<field_name>[.a-zA-Z0-9_-]+)", re.IGNORECASE)
 operator_pat = re.compile(r"^(?P<operator>[a-zA-Z0-9=><!_-]+)")
-operators_to_fun = {
-    'contains': [1, None],
-    '!contains': [1, None],
-    'contains_ic': [1, None],
-    '!contains_ic': [1, None],
-    'startswith': [1, None],
-    '!startswith': [1, None],
-    'startswith_ic': [1, None],
-    'endswith': [1, None],
-    '!endswith_ic': [1, None],
-    '=': [1, None],
-    '!=': [1, None],
-    '>': [1, None],
-    '<': [1, None],
-    '>=': [1, None],
-    '<=': [1, None]
+
+valid_operators_fun_names = {
+    'in',
+    '!in',
+    'contains',
+    '!contains',
+    'contains_ic',
+    '!contains_ic',
+    'startswith',
+    '!startswith',
+    'startswith_ic',
+    'endswith',
+    '!endswith_ic',
+    '==',
+    '!=',
+    '>',
+    '<',
+    '>=',
+    '<='
 }
 
-def parse(raw):
-    raw = raw.strip()
-    module_name = module_name_pat.match(raw).group("module_name")
+valid_limiter_fun_names = {
+    'limit',
+    'offset',
+    'from',
+    'first',
+    'last'
+}
+
+valid_producer_fun_names = {
+    'one'
+}
+
+
+def parse(filter_text, values):
+    filtered_vlaue_s = values.copy()
+    filter_text = filter_text.strip()
+    module_name = module_name_pat.match(filter_text).group("module_name")
     print(module_name)
-    query_str = raw[len(module_name):]
+    query_str = filter_text[len(module_name):]
     query_str = query_str.lstrip(" |\r\n\t")
     print(query_str)
 
@@ -44,10 +62,13 @@ def parse(raw):
         if query.startswith('@'):
             # call producer
             producer_name = query[1:]
+            filtered_vlaue_s = __filter_funs.producers[producer_name](filtered_vlaue_s)
+            break
         elif query.startswith(':'):
             # call limitter
             limiter_name = query[1:].split(' ')[0]
-            limiter_operators = (eval(x) for x in query[1:].split(' ')[1:])
+            limiter_operands = (eval(x) for x in query[1:].split(' ')[1:])
+            filtered_vlaue_s = __filter_funs.limiters[limiter_name](filtered_vlaue_s, limiter_operands)
         else:  # operator
             field_name = field_name_pat.match(query).group('field_name')
             _query = query[len(field_name):].strip()
@@ -55,64 +76,167 @@ def parse(raw):
             _query = _query[len(operator):].strip()
             operand = eval(_query)
             print("Field name: %s, operator: %s, operand: %s" % (field_name, operator, operand))
+            filtered_vlaue_s = __filter_funs.operators[operator](filtered_vlaue_s, field_name, operand)
+    return filtered_vlaue_s
 
-parse(v)
+parse(v, [])
+
+_FilterFuns = namedtuple('FilterFun', ['operators', 'limiters', 'producers'])
+__filter_funs = _FilterFuns(operators={}, limiters={}, producers={})
+
+
+# decorators
+def operator_decorator(operator_fun):
+    def inner_wrapper(operator_name):
+        assert operator_name not in valid_operators_fun_names, "Invalid operator name"
+        assert operator_name not in __filter_funs.operators, "Duplicate operator fun"
+        __filter_funs.operators[operator_name] = operator_fun
+        return operator_fun
+    return inner_wrapper
+
+
+def limiter_decorator(limiter_fun):
+    def inner_wrapper(limiter_name):
+        assert limiter_name not in valid_limiter_fun_names, "Invalid limiter name"
+        assert limiter_name not in __filter_funs.limiters, "Duplicate limiter"
+        __filter_funs.limiters[limiter_name] = limiter_fun
+        return limiter_fun
+    return inner_wrapper
+
+
+def producer_decorator(producer_fun):
+    def inner_wrapper(producer_name):
+        assert producer_name not in valid_producer_fun_names, "Invalid producer name"
+        assert producer_name not in __filter_funs.producers, "Duplicate producers"
+        __filter_funs.producers[producer_name] = producer_fun
+        return producer_fun
+    return inner_wrapper
 
 # operators
+
+
+@operator_decorator('==')
 def operator_eq():
     pass
 
+
+@operator_decorator('!=')
 def operator_neq():
     pass
 
+
+@operator_decorator('>')
 def operator_gt():
     pass
 
+
+@operator_decorator('<')
 def operator_lt():
     pass
 
+
+@operator_decorator('>=')
 def operator_gteq():
     pass
 
+
+@operator_decorator('<=')
 def operator_lteq():
     pass
 
+
+@operator_decorator('contains')
 def operator_contains():
     pass
 
+
+@operator_decorator('!contains')
+def operator_not_contains():
+    pass
+
+
+@operator_decorator('contains_ic')
 def operator_contains_ic():
     pass
 
+
+@operator_decorator('!contains_ic')
+def operator_not_contains_ic():
+    pass
+
+
+@operator_decorator('startswith')
 def operator_startswith():
     pass
 
+
+@operator_decorator('!startswith')
+def operator_not_startswith():
+    pass
+
+
+@operator_decorator('startswith_ic')
 def operator_startswith_ic():
     pass
 
+
+@operator_decorator('!startswith_ic')
+def operator_not_startswith_ic():
+    pass
+
+
+@operator_decorator('endswith')
 def operator_endswith():
     pass
 
+
+@operator_decorator('!endswith')
+def operator_not_endswith():
+    pass
+
+
+@operator_decorator('endswith_ic')
 def operator_endswith_ic():
     pass
 
 
-# limiter
+@operator_decorator('!endswith_ic')
+def operator_not_endswith_ic():
+    pass
 
+
+# limiter
+@limiter_decorator('limit')
 def limiter_limit():
     pass
+
+
+@limiter_decorator('offset')
 def limiter_offset():
     pass
+
+
+@limiter_decorator('from')
 def limiter_from():
     pass
+
+
+@limiter_decorator('first')
 def limiter_first():
     pass
+
+
+@limiter_decorator('last')
 def limiter_last():
     pass
 
+
 # producer
+# @producer_decorator('list')
+# def producer_list():
+#     pass
 
-def producer_list():
-    pass
 
+@producer_decorator('one')
 def producer_one():
     pass
