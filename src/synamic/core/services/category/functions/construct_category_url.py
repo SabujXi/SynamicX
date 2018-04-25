9c1f7bb6648e5759b4c080c4585b4dff0540fc31
sabuj <md.sabuj.sarker@gmail.com>
2018-04-25 15:10:20 +0600

    Tags & Categories content no-extension-file fixed with directory based index.html

--- src/synamic/core/services/category/functions/construct_category_url.py
+++ src/synamic/core/services/category/functions/construct_category_url.py
@@ -2,4 +2,4 @@ from synamic.core.urls.url import ContentUrl
 
 
 def construct_category_url(synamic, category):
-    return ContentUrl(synamic, '_/category/' + category.id)
+    return ContentUrl(synamic, '_/category/' + category.id, append_slash=True)
--- src/synamic/core/services/tags/functions/construct_tag_url.py
+++ src/synamic/core/services/tags/functions/construct_tag_url.py
@@ -2,4 +2,4 @@ from synamic.core.urls.url import ContentUrl
 
 
 def construct_tag_url(synamic, tag):
-    return ContentUrl(synamic, '_/tag/' + tag.id)
+    return ContentUrl(synamic, '_/tag/' + tag.id, append_slash=True)
