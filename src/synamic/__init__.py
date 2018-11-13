61bd0d86b8efee11cecdd840541369ec87724033
sabuj <md.sabuj.sarker@gmail.com>
2018-11-13 19:36:04 +0600

    Nil introduced, but not used yet: synamic.Nil

---
+++ src/synamic/__init__.py
@@ -0,0 +1,2 @@
+from .core.standalones.classes.nil import Nil
+from .core.synamic import Synamic
---
+++ src/synamic/core/standalones/classes/nil.py
@@ -0,0 +1,47 @@
+__all__ = ['Nil']
+
+
+class _Nil:
+    __instance_count = 0
+
+    def __init__(self):
+        assert self.__instance_count == 0, f'Only one instance of Nil can exist in the whole system. Do not try to ' \
+                                           f'instantiate by yourself. Use the one synamic.Nil'
+
+        self.__instance_count += 1
+
+    def __eq__(self, other):
+        return id(other) == id(self)
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __gt__(self, other):
+        return False
+
+    def __lt__(self, other):
+        return False
+
+    def __ge__(self, other):
+        return self.__eq__(other)
+
+    def __le__(self, other):
+        return self.__eq__(other)
+
+    def __contains__(self, item):
+        return False
+
+    def __hash__(self):
+        return id(self)
+
+    def __bool__(self):
+        return False
+
+    def __str__(self):
+        return ''
+
+    def __repr__(self):
+        return ''
+
+
+Nil = _Nil()
